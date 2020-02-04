5e491734705a1468e8fd0b37d8b1f5808769ad66
genceyalcin <genceyalcin@vt.edu>
2020-02-03 22:15:43 -0500

    JS

--- app/static/JS/chat.js
+++ app/static/JS/chat.js
@@ -1,1 +1,14 @@
+$(document).ready( function () {
 
+	var socket = io();
+
+	socket.on('connect', function() {
+		console.log('I connected');
+		socket.emit('test', {data: 'Your mom gay'});
+	});
+
+	socket.on('test', function(data) {
+		console.log(data.data);
+	})
+
+})
