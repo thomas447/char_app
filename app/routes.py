8a2bed56f4fd7c6b695f6f16cd913729298de4b2
Thomas Szydlowski <thomas447@vt.edu>
2020-02-04 02:56:12 +0000

    initial socket ????

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
-from app import app
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '...'
+
+socketio = SocketIO(app)
 
 @app.route('/')
 @app.route('/index')
 def index():
 	#return render_template("index.html")
 	return "hello world"
+
+@socketio.on('connect')
+def handle_connect():
+	print("connected")
+
+if __name__ == "__main__":
+	socketio.run(app)
